f45b4ae5e6837b10eb66362bd6a54cb80f547936
Sander Huijbers <sander@sandkar.nl>
2019-07-15 15:51:38 +0200

    initial commit

---
+++ scripts/favorieten.js
@@ -0,0 +1,143 @@
+random_number = Math.floor(Math.random() * 17 + 1);
+console.log(random_number);
+// Loads the appropriate image and text color based on random number.
+if (random_number == 1) {
+image = "./images/backgrounds/ravi2.jpg";
+}
+if (random_number == 2) {
+image = "./images/backgrounds/blue_bg.jpg";
+}
+if (random_number == 3) {
+image = "./images/backgrounds/gem_bg.jpg";
+}
+if (random_number == 4) {
+image = "./images/backgrounds/frost_bg.jpg";
+}
+if (random_number == 5) {
+image = "./images/backgrounds/marble_bg.jpg";
+}
+if (random_number == 6) {
+image = "./images/backgrounds/blue-sky_bg.jpg";
+}
+if (random_number == 7) {
+image = "./images/backgrounds/popcorn_bg.jpg";
+}
+if (random_number == 8) {
+image = "./images/backgrounds/pinkrose_bg.jpg";
+}
+if (random_number == 9) {
+image = "./images/backgrounds/stone_bg.jpg";
+}
+if (random_number == 10) {
+image = "./images/backgrounds/stone2_bg.jpg";
+}
+if (random_number == 11) {
+image = "./images/backgrounds/camouflage_bg.jpg";
+}
+if (random_number == 12) {
+image = "./images/backgrounds/fire_bg.jpg";
+}
+if (random_number == 13) {
+image = "./images/backgrounds/bricks_bg.jpg";
+}
+if (random_number == 14) {
+image = "./images/backgrounds/blossom_bg.jpg";
+}
+if (random_number == 15) {
+image = "./images/backgrounds/strawberry_bg.jpg";
+}
+if (random_number == 16) {
+image = "./images/backgrounds/ravi.jpg";
+}
+if (random_number == 17) {
+image = "./images/backgrounds/orange_bg.jpg";
+}
+
+var box0101 = ' <h2>SandKar sites</h2>\
+<ul>\
+<li><a href="https://frw.sandkar.nl">Fotorw</a></li>\
+<li><a href="https://www.sandkar.nl/canada">Canada page</a></li>\
+<li><a href="https://www.sandkar.nl">Feyenoordpage</a></li>\
+<li><a href="https://www.sandkar.nl/favorieten/hoekvh.htm">Hoek van Holland</a></li>\
+<li><a href="https://maassluis.sandkar.nl">Maassluis</a></li>\
+<li><a href="https://kookieook.sandkar.nl">Kookieook</a></li>\
+<li><a href="https://www.sandkar.nl/holiday">Sandkar\'s holidaypage</a></li>\
+<li><a href="https://www.sandkar.nl/clubsites">Offici&euml;le Club Sites</a></li>\
+<li><a href="https://www.sandkar.nl/rasta">Planet Rasta</a></li>\
+<li><a href="https://sandkar.nl/awstats">Statistieken</a></li>\
+<li><a href="https://www.sandkar.nl/thuiswerken">Thuiswerken</a></li>\
+<li><a href="https://www.sandkar.nl/webdeveloper">Web developer</a></li>\
+</ul>'; 
+var box0102 = '<h2>Streaming Audio</h2>\
+<ul>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.spotify.com/">Spotify</a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.radio1.nl">Radio 1</a></div><div class="radiostreambuttons"><a href="https://radioplayer.omroep.nl/radio1-default"><img src="images/listen.png" alt="Luisteren" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.radio2.nl">Radio 2</a></div><div class="radiostreambuttons"><a href="https://radioplayer.omroep.nl/radio2/"><img src="images/listen.png" alt="Luisteren naar Radio2" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.3fm.nl/">Radio 3</a></div><div class="radiostreambuttons"><a href="https://radioplayer.omroep.nl/3fm/"><img src="images/listen.png" alt="Luisteren naar 3FM" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.radio4.nl">Radio 4</a></div><div class="radiostreambuttons"><a href="https://radioplayer.omroep.nl/radio4/"><img src="images/listen.png" alt="Luisteren naar Radio4" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.nporadio5.nl/">Radio 5</a></div><div class="radiostreambuttons"><a href="https://radioplayer.omroep.nl/radio5/"><img src="images/listen.png" alt="Luisteren naar Radio5" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.radio6.nl">Radio 6</a></div><div class="radiostreambuttons"><a href="https://radioplayer.omroep.nl/radio6/" target="_blank"><img src="images/listen.png" alt="Luisteren naar Radio5" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.bnr.nl">BNR</a></div><div class="radiostreambuttons"><a href="https://www.bnr.nl/player/live"><img src="images/listen.png" alt="Luisteren naar BNR" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.radiorijnmond.nl">RTV-Rijnmond</a></div><div class="radiostreambuttons"><a href="https://tunein.com/radio/Radio-Rijnmond-934-s17530/"><img src="images/listen.png" alt="Luisteren naar Radio Rijnmond" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.radio538.nl/">Radio538</a></div><div class="radiostreambuttons"><a href="https://82.201.100.9:8000/radio538.m3u"><img src="images/listen.png"alt="Luisteren naar Radio538" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.slamfm.nl">Slam FM</a></div><div class="radiostreambuttons"><a href="https://82.201.100.23/slamfm.m3u"><img src="images/listen.png"alt="Luisteren naar FreshFM" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.fresh.fm/">Fresh FM</a></div><div class="radiostreambuttons"><a href="https://www.fresh.fm/media/audio/freshfm.asx"><img src="images/listen.png"alt="Luisteren naar 3FM" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://tunein.com/">TuneIn</a></div><div class="radiostreambuttons"><a href="https://tunein.com/user/sander9264/"><img src="images/listen.png"alt="Luisteren naar sandkars lijst" /></a></div></li>\
+<li class="radiostream"><div class="radiostreamwebpage"><a href="https://www.nederland.fm/">Nederland.fm</a></div></li>\
+</ul>';
+var box0201 = '<h2>Google</h2>\
+<ul>\
+    <li><a href="https://www.google.com/calendar">Agenda</a></li>\
+    <li><a href="https://images.google.nl">Afbeeldingen</a></li>\
+    <li><a href="https://www.gmail.com">Gmail</a></li>\
+    <li><a href="https://maps.google.nl">Maps</a></li>\
+    <li><a href="https://photos.google.com/">Foto\'s</a></li>\
+    <li><a href="https://news.google.nl/">Nieuws</a></li>\
+    <li><a href="https://translate.google.nl">Vertalen</a></li>\
+    <li><a href="https://shoppinglist.google.com/lists">Boodschappenlijsten</a></li>\
+    <li><a href="https://app.sketchup.com/app?hl=nl">Sketchup</a></li>\
+    <li><a href="https://video.google.nl">Video</a></li>\
+</ul>';
+var box0202 = '<h2>Het weer</h2>\
+<ul>\
+    <li><a href="https://www.weeronline.nl">WeerOnline</a></li>\
+    <li><a href="https://www.sandkar.nl/favorieten/hoekvh.htm">Hoek van Holland</a></li>\
+    <li><a href="https://maassluis.sandkar.nl">Maassluis</a></li>\
+    <li><a href="https://www.weer.nl">Weer.nl</a></li>\
+    <li><a href="https://www.buienradar.nl">Buienradar.nl</a></li>\
+    <li><a href="https://www.meteox.com">Buienradar west europa</a></li>\
+    <li><a href="https://www.boatus.com/hurricanes/tracking.asp">Tornado\'s</a>(U.S.A.)<br /></li>\
+    <li><a href="https://www.nhc.noaa.gov/">National Hurricane Center</a></li>\
+</ul>';
+var box0203 = '<h2>Media info</h2>\
+<ul>\
+    <li><a href="https://www.imdb.com">IMDB</a></li>\
+    <li><a href="https://www.trakt.tv">Trakt</a></li>\
+</ul>';
+var fakeBox7 = '<h2>7Linux Distributies</h2>';
+var fakeBox8 = '<h2>8Linux Distributies</h2>';
+var fakeBox9 = '<h2>9Linux Distributies</h2>';
+var fakeBox10 = '<h2>10Linux Distributies</h2>';
+var fakeBox11 = '<h2>11Linux Distributies</h2>';
+var fakeBox12 = '<h2>12Linux Distributies</h2>';
+var fakeBox13 = '<h2>13Linux Distributies</h2>';
+var fakeBox14 = '<h2>14Linux Distributies</h2>';
+var box0105 = '<h2>Linux Distributies</h2> \
+<ul> \
+<li><a href="https://linuxmint.com/">LinuxMint</a></li> \
+<li><a href="https://www.ubuntu.com/">Ubuntu</a></li> \
+<li><a href="https://www.debian.org/index.nl.html">Debian</a></li> \
+<li><a href="https://ubuntu-mate.org/">Ubuntu mate</a></li> \
+<li><a href="https://xubuntu.org/">Xubuntu</a></li> \
+<li><a href="https://elementary.io/">Elementary OS</a></li> \
+<li><a href="https://nl.opensuse.org/">Open Suse</a></li> \
+<li><a href="https://getfedora.org/">Fedora</a></li> \
+<li><a href="https://www.archlinux.org/">Arch</a></li> \
+<li><a href="https://www.redhat.com/en">Redhat</a></li> \
+</ul>';
+var fakeBox16 = '<h2>16Linux Distributies</h2>';
+var fakeBox17 = '<h2>17Linux Distributies</h2>';
+var fakeBox18 = '<h2>18Linux Distributies</h2>';
+var fakeBox19 = '<h2>19Linux Distributies</h2>';
+var fakeBox20 = '<h2>20Linux Distributies</h2>';
+var hallo = ('fakebox' + random_number);
